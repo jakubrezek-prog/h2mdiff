a7de774b7df3b90b25312e63e39755c674701a9d
rezekj <jakub.rezek@gmail.com>
2026-03-13 00:01:38 +0100

    initial commit

---
+++ src/main/java/com/jr/util/h2mdiff/App.java
@@ -0,0 +1,10 @@
+package com.jr.util.h2mdiff;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/jr/util/h2mdiff/AppTest.java
@@ -0,0 +1,19 @@
+package com.jr.util.h2mdiff;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
